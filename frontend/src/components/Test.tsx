e6b36d870cc2f172f3710d8b458a60e036c71018
pytest5 <pytest514@gmail.com>
2024-09-26 14:47:18 +0800

    chore: removed unused things

--- frontend/src/components/Test.tsx
+++
@@ -1,8 +0,0 @@
-import { useFetch } from "@/hooks/use-fetch";
-import React from "react";
-import { TotalsOverview } from "./dashboard/TotalsOverview";
-import { ItemsBreakDown } from "./dashboard/ItemsBreakdown-oldd";
-
-export const Test = () => {
-  // <ItemsBreakDown />;
-};
--- frontend/src/components/UserAvatar.tsx
+++ frontend/src/components/UserAvatar.tsx
@@ -4,7 +4,7 @@ import { useUser } from "./UserProvider";
 
 export const UserAvatar = ({ className }: { className?: string }) => {
   const { user } = useUser();
-
+  console.log("user in userAvatar", user);
   return (
     <Avatar className={`h-16 w-16 border-gray-200 ${className}`}>
       <AvatarImage src="/placeholder-avatar.jpg" alt={user?.username[0]} />
