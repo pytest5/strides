685b87769c57b8dda9ad32084396e78dcb2b4556
pytest5 <pytest514@gmail.com>
2024-09-26 15:44:48 +0800

    feat: add strides page

--- frontend/src/pages/AddStridesPage.tsx
+++ frontend/src/pages/AddStridesPage.tsx
@@ -1,84 +1,170 @@
-"use client";
-
+import React from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { useState } from "react";
-import { useForm, Controller } from "react-hook-form";
-import {
-  ArrowLeft,
-  HelpCircle,
-  Minus,
-  Plus,
-  Package,
-  Settings,
-  Users,
-  BarChart,
-  Search,
-} from "lucide-react";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+import { Minus, Plus } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Badge } from "@/components/ui/badge";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Separator } from "@/components/ui/separator";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+} from "@/components/ui/form";
+import { useMutation } from "@tanstack/react-query";
+import { submitStride, Location } from "@/services/stridesService";
+import { useUser } from "@/components/UserProvider";
+import { useGeoLocation } from "@/hooks/use-geo-location";
+import { TeamComboBox } from "@/components/TeamComboBox";
+import { useTriggerToast } from "@/hooks/use-trigger-toast";
+import { Link } from "react-router-dom";
+
+export type AddStrideFormDataType = z.infer<typeof formSchema>;
+
+const formSchema = z
+  .object({
+    glass: z.number().min(0),
+    plastic: z.number().min(0),
+    metal: z.number().min(0),
+    bags: z.number().min(0),
+    cups: z.number().min(0),
+    packaging: z.number().min(0),
+    tissue: z.number().min(0),
+    cigarette: z.number().min(0),
+    others: z.number().min(0),
+    team: z.optional(z.string()),
+  })
+  .refine((data) => Object.values(data).some((value) => value !== 0), {
+    message: "Please select at least one item.",
+    path: ["_errors"], // ['root'] doesnt work here lol......
+  });
+
+interface MutationArgs {
+  data: AddStrideFormDataType;
+  jwtToken: string;
+  location: Location;
+}
 
 const wasteCategories = [
-  { name: "Glass", icon: "🍾" },
-  { name: "Plastic", icon: "🥤" },
-  { name: "Metal", icon: "🥫" },
-  { name: "Bags", icon: "🛍️" },
-  { name: "Cups", icon: "☕" },
-  { name: "Packaging", icon: "📦" },
-  { name: "Tissue", icon: "🧻" },
-  { name: "Cigarette", icon: "🚬" },
-  { name: "Others", icon: "🗑️" },
+  { name: "Glass", icon: "/glassBottle.png" },
+  { name: "Plastic", icon: "/plasticBottle.png" },
+  { name: "Metal", icon: "/metalCan.png" },
+  { name: "Bags", icon: "/bag.png" },
+  { name: "Cups", icon: "/cups.png" },
+  { name: "Packaging", icon: "/packaging.png" },
+  { name: "Tissue", icon: "/tissue.png" },
+  { name: "Cigarette", icon: "/cigarette.png" },
+  { name: "Others", icon: "/others.png" },
 ];
 
-export default function AddStridesPage() {
-  const { control, handleSubmit, watch, setValue } = useForm({
-    defaultValues: wasteCategories.reduce(
-      (acc, category) => ({ ...acc, [category.name.toLowerCase()]: 0 }),
-      {}
-    ),
-  });
-  const [selectedCategory, setSelectedCategory] = useState("");
+const defaultValues = {
+  ...wasteCategories.reduce(
+    (a, c) => ({ ...a, [c.name.toLowerCase()]: 0 }),
+    {}
+  ),
+  teams: "",
+};
 
-  const onSubmit = (data) => {
-    console.log(data);
-    // Here you would typically send the data to your backend
+export default function AddStridesPage() {
+  const [selectedCategory, setSelectedCategory] = useState("");
+  const { jwtToken } = useUser();
+  const { location } = useGeoLocation();
+  const triggerToast = useTriggerToast();
+  console.log(triggerToast);
+
+  const mutation = useMutation({
+    mutationFn: ({ newData, jwtToken, location }: MutationArgs) => {
+      return submitStride(newData, jwtToken, location);
+    },
+    onSuccess: (data, variable, context) => {
+      triggerToast("submit");
+    },
+    // onSuccess: (data, variable, context) => {
+    //   triggerToast("submit");
+    // },
+    onError: (error) => {
+      "submitted";
+      console.error("Error submitting form:", error);
+    },
+  });
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues,
+  });
+
+  const {
+    watch,
+    setValue,
+    formState: { errors },
+    reset,
+  } = form;
+
+  const onSubmit = (data: AddStrideFormDataType) => {
+    const { glass, plastic, metal, ...rest } = data;
+    const newData = {
+      ...rest,
+      team: watchTeam,
+      "glass bottle": data["glass"],
+      "plastic bottle": data["plastic"],
+      "metal can": data["metal"],
+    };
+    console.log("submitting", newData);
+    if (Object.values(data).every((value) => value === 0)) {
+      console.log("Form is empty, preventing submission");
+      return;
+    }
+    const res = mutation.mutate({ newData, jwtToken, location });
+    console.log("Form submitted successfully");
+    reset(defaultValues);
+    return res;
   };
 
-  const watchAllFields = watch();
-  const totalItems = Object.values(watchAllFields).reduce(
-    (sum, value) => sum + (value || 0),
-    0
-  );
+  const watchTeam = watch("team"); // Watch team selection
 
-  const handleCategoryClick = (category) => {
-    const lowerCaseCategory = category.toLowerCase();
+  const watchAllFields = watch();
+
+  console.log("ERRORS", errors);
+
+  const totalItems = Object.values(watchAllFields)
+    .filter((value) => typeof value === "number")
+    .reduce((sum, value) => sum + (value || 0), 0);
+
+  const handleCategoryClick = (category: string) => {
+    const lowerCaseCategory = category.toLowerCase() as keyof z.infer<
+      typeof formSchema
+    >;
     const currentValue = watchAllFields[lowerCaseCategory] || 0;
     setValue(lowerCaseCategory, currentValue + 1);
     setSelectedCategory(lowerCaseCategory);
   };
 
   return (
-    <div className=" flex flex-col h-full flex-1 md:flex-row bg-gray-100 text-foreground">
+    <div className="flex flex-col md:flex-row bg-gray-100 text-foreground h-full ">
       {/* Strides Summary */}
-      <aside className="grid p-5 py-0 sm:flex-col w-full md:w-64 md:p-6 border-r">
-        <h2 className="text-sm font-semibold text-gray-700 pb-4">
+      <aside className="grid sm:flex p-5 py-0 sm:flex-col w-full md:w-56 md:p-6 md:pt-9 border-r ">
+        <h2 className="text-sm font-semibold text-gray-900 pb-4 md:text-center ">
           Current Stride Summary
         </h2>
         {/* Points / Items / Time / Dist */}
-        <div className="flex flex-1 md:flex-col items-stretch text-center bg-slate-200 md:bg-gray-100 rounded-md py-3 pr-4 md:border-none">
-          <div className="border-r-2  border-slate-300 flex-1 md:border-r-0">
+        <div className="flex md:flex-col md:gap-4 md:items-center items-stretch text-center md:bg-gray-100 rounded-md py-3 pr-4 md:border-none md:pr-0">
+          <div className="border-r-2 border-slate-200 flex-1 md:border-r-0">
             <Label className="font-normal text-gray-500">Points</Label>
             <p className="text-md md:text-2xl font-bold ">{totalItems}</p>
           </div>
           <Separator className="hidden md:block" />
-          <div className="border-r-2 border-slate-300 flex-1 md:border-r-0">
+          <div className="border-r-2 border-slate-200 flex-1 md:border-r-0">
             <Label className="font-normal text-gray-500">Items</Label>
             <p className="text-md md:text-2xl font-bold">{totalItems}</p>
           </div>
           <Separator className="hidden md:block" />
-          <div className="border-r-2  border-slate-300 flex-1 md:border-r-0">
+          <div className="border-r-2 border-slate-200 flex-1 md:border-r-0">
             <Label className="font-normal text-gray-500">Time</Label>
             <p className="text-md md:text-2xl font-bold">15m</p>
           </div>
@@ -90,112 +176,174 @@ export default function AddStridesPage() {
         </div>
       </aside>
 
-      <main className="flex-1 p-4 md:p-6">
-        <h1 className="text-sm font-semibold text-gray-700 pb-4 md:text-3xl md:font-bold md:mb-6">
-          Current Stride
-        </h1>
-        <form onSubmit={handleSubmit(onSubmit)} className="space-y-6">
-          <Card className="">
-            <CardHeader className="hidden md:block py-4 md:p-6">
-              <CardTitle>Waste Categories</CardTitle>
-            </CardHeader>
-            <CardContent className="bg-slate-200 pt-4 md:pt-0 grid grid-cols-3 lg:grid-cols-4 gap-4">
-              {wasteCategories.map((category) => (
-                <Button
-                  key={category.name}
-                  variant={
-                    watchAllFields[category.name.toLowerCase()] > 0
-                      ? "default"
-                      : "outline"
-                  }
-                  className="h-24 flex bg-gray-100 flex-col justify-center items-center relative"
-                  onClick={() => handleCategoryClick(category.name)}
-                >
-                  <span className="text-2xl mb-2">{category.icon}</span>
-                  <span className="text-sm">{category.name}</span>
-                  {watchAllFields[category.name.toLowerCase()] > 0 && (
-                    <Badge
-                      className="absolute top-1 right-1"
-                      variant="secondary"
-                    >
-                      {watchAllFields[category.name.toLowerCase()]}
-                    </Badge>
-                  )}
-                </Button>
-              ))}
-            </CardContent>
-          </Card>
-
-          <Card>
-            <CardHeader>
-              <CardTitle>Quantity</CardTitle>
-            </CardHeader>
-            <CardContent>
-              <div className="flex items-center space-x-4">
-                <Button
-                  type="button"
-                  variant="outline"
-                  size="icon"
-                  onClick={() =>
-                    selectedCategory &&
-                    setValue(
-                      selectedCategory,
-                      Math.max(0, (watchAllFields[selectedCategory] || 0) - 1)
-                    )
-                  }
-                >
-                  <Minus className="h-4 w-4" />
-                </Button>
-                <Controller
-                  name={selectedCategory}
-                  control={control}
-                  render={({ field }) => (
-                    <Input
-                      {...field}
-                      type="number"
-                      className="w-20 text-center"
-                      value={field.value || 0}
-                      onChange={(e) =>
-                        field.onChange(
-                          Math.max(0, parseInt(e.target.value) || 0)
+      <main className="flex-1 flex flex-col h-full p-4 pt-0 md:py-6 md:px-8 overflow-hidden ">
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="flex flex-col gap-4 h-full "
+          >
+            <div className="flex-1 overflow-auto ">
+              {/* Current stride */}
+              <h1 className="text-sm pt-2 py-3 font-semibold text-gray-900  md:text-3xl md:font-bold md:mb-2 md:border-b">
+                Current Stride
+              </h1>
+              <h2 className="hidden md:block text-sm pt-5 py-3 md:pt-1 md:pb-1 font-semibold text-gray-900 md:text-gray-600 md:text-lg md:font-bold md:mb-0">
+                Waste categories
+              </h2>
+              <Card className="border-0 shadow-none md:shadow-sm md:border-2 ">
+                <CardContent className="p-0 md:p-4 grid grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 md:pt-4 bg-gray-100 rounded-xl border-0 shadow-none md:bg-gray-50 ">
+                  {wasteCategories.map((category) => (
+                    <FormField
+                      key={category.name}
+                      control={form.control}
+                      name={
+                        /*  https://www.typescriptlang.org/docs/handbook/2/keyof-types.html */
+                        category.name.toLowerCase() as keyof z.infer<
+                          typeof formSchema
+                        >
+                      }
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormControl>
+                            <Button
+                              type="button"
+                              // variant={field.value > 0 ? "default" : "outline"}
+                              variant={"outline"}
+                              className="h-24 w-full flex bg-slate-100 flex-col justify-center items-center rounded-lg relative shadow-md"
+                              onClick={() => handleCategoryClick(category.name)}
+                            >
+                              <img
+                                src={category.icon}
+                                alt={category.name}
+                                className="w-8 h-8 mb-2"
+                              />
+                              <span className="text-sm ">{category.name}</span>
+                              {field.value > 0 && (
+                                <Badge
+                                  className="absolute top-1 right-1 bg-slate-700"
+                                  variant="default"
+                                >
+                                  {field.value}
+                                </Badge>
+                              )}
+                            </Button>
+                          </FormControl>
+                        </FormItem>
+                      )}
+                    />
+                  ))}
+                </CardContent>
+              </Card>
+              {errors._errors && (
+                <div className="text-destructive">{errors._errors.message}</div>
+              )}
+              {/* Quantity */}
+              <h2 className=" text-sm pt-5 py-3 md:pt-2 md:pb-1 font-semibold text-gray-900 md:text-gray-600 md:text-lg md:font-bold md:mb-0">
+                Quantity
+              </h2>
+              <Card>
+                <CardContent className="p-4 bg-gray-50 rounded-lg">
+                  <div className="flex items-center space-x-4">
+                    <Button
+                      type="button"
+                      variant="outline"
+                      size="icon"
+                      onClick={() =>
+                        selectedCategory &&
+                        setValue(
+                          selectedCategory as keyof z.infer<typeof formSchema>,
+                          Math.max(
+                            0,
+                            (watchAllFields[
+                              selectedCategory as keyof z.infer<
+                                typeof formSchema
+                              >
+                            ] || 0) - 1
+                          )
                         )
                       }
+                    >
+                      <Minus className="h-4 w-4" />
+                    </Button>
+                    <FormField
+                      key={selectedCategory}
+                      control={form.control}
+                      /*  https://www.typescriptlang.org/docs/handbook/2/keyof-types.html */
+                      name={
+                        selectedCategory as keyof z.infer<typeof formSchema>
+                      }
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormControl>
+                            <Input
+                              {...field}
+                              type="number"
+                              className="w-20 text-center"
+                              value={field.value || 0}
+                              onChange={(e) =>
+                                field.onChange(
+                                  Math.max(0, parseInt(e.target.value) || 0)
+                                )
+                              }
+                            />
+                          </FormControl>
+                        </FormItem>
+                      )}
                     />
-                  )}
-                />
-                <Button
-                  type="button"
-                  variant="outline"
-                  size="icon"
-                  onClick={() =>
-                    selectedCategory &&
-                    setValue(
-                      selectedCategory,
-                      (watchAllFields[selectedCategory] || 0) + 1
-                    )
-                  }
-                >
-                  <Plus className="h-4 w-4" />
-                </Button>
-                <Label>
-                  {selectedCategory
-                    ? selectedCategory.charAt(0).toUpperCase() +
-                      selectedCategory.slice(1)
-                    : "Select a category"}
-                </Label>
-              </div>
-            </CardContent>
-          </Card>
-
-          <div className="flex space-x-4">
-            <Button type="button" variant="outline" className="flex-1">
-              End Stride
-            </Button>
-            <Button type="submit" className="flex-1">
-              Save Pickup
-            </Button>
-          </div>
-        </form>
+                    <Button
+                      type="button"
+                      variant="outline"
+                      size="icon"
+                      onClick={() =>
+                        selectedCategory &&
+                        setValue(
+                          selectedCategory as keyof z.infer<typeof formSchema>,
+                          (watchAllFields[
+                            selectedCategory as keyof z.infer<typeof formSchema>
+                          ] || 0) + 1
+                        )
+                      }
+                    >
+                      <Plus className="h-4 w-4" />
+                    </Button>
+                    <FormLabel>
+                      {selectedCategory
+                        ? selectedCategory.charAt(0).toUpperCase() +
+                          selectedCategory.slice(1)
+                        : "Select a category"}
+                    </FormLabel>
+                  </div>
+                </CardContent>
+              </Card>
+              {/* Add to team */}
+              <h2 className=" text-sm pt-5 py-3 md:pt-2 md:pb-1 font-semibold text-gray-900 md:text-gray-600 md:text-lg md:font-bold md:mb-0">
+                Team
+              </h2>
+              <TeamComboBox
+                value={watchTeam}
+                setValue={(value) => setValue("team", value)}
+              />
+              {/* Actions segment */}
+            </div>
+            <div className="flex space-x-4 mt-auto ">
+              <Button
+                asChild
+                type="button"
+                variant="outline"
+                className="flex-1"
+              >
+                <Link to="/">End Stride</Link>
+              </Button>
+              <Button
+                type="submit"
+                className="flex-1"
+                disabled={mutation.isPending}
+              >
+                {mutation.isPending ? "Saving..." : "Save Pickup"}
+              </Button>
+            </div>
+          </form>
+        </Form>
       </main>
     </div>
   );
