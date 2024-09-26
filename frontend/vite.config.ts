55f6e1b06e9d1be98531107e311c12744cfcb1eb
pytest5 <pytest514@gmail.com>
2024-09-26 19:04:37 +0800

    fix: update proxy

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -12,7 +12,8 @@ export default defineConfig({
   },
   server: {
     proxy: {
-      "/api": "http://localhost:3000",
+      // "/api": "http://localhost:3000",
+      "/api": "https://strides-7oqw.onrender.com",
     },
   },
 });
